bd144e9e64940c0c67e9ef139c64d90ea07f3325
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-07-09 00:18:51 +0300

    ascascscxzc

--- parcer/index.js
+++ parcer/index.js
@@ -1,155 +1,112 @@
-console.log("Hello world")
+console.log("Hello world");
 
-const puppeteer = require('puppeteer');
-const kurs = '37,2';
+const puppeteer = require("puppeteer");
 
+const arrLinkSkay14 = [
+  "https://skay.ua/uk/iphone-14/58753-apple-iphone-14-128gb-blue.html",
+  "https://skay.ua/uk/iphone-14/58757-apple-iphone-14-128gb-product-red.html",
+  "https://skay.ua/uk/iphone-14/58755-apple-iphone-14-128gb-midnight.html",
+  "https://skay.ua/uk/iphone-14/58756-apple-iphone-14-128gb-starlight.html",
+  "https://skay.ua/uk/iphone-14/58754-apple-iphone-14-128gb-purple.html",
+  "https://skay.ua/uk/iphone-14/61506-apple-iphone-14-128gb-yellow.html",
+  "https://skay.ua/uk/iphone-14/58758-apple-iphone-14-256gb-blue.html",
+  "https://skay.ua/uk/iphone-14/58762-apple-iphone-14-256gb-product-red.html",
+  "https://skay.ua/uk/iphone-14/58760-apple-iphone-14-256gb-midnight.html",
+  "https://skay.ua/uk/iphone-14/58761-apple-iphone-14-256gb-starlight.html",
+  "https://skay.ua/uk/iphone-14/58759-apple-iphone-14-256gb-purple.html",
+  "https://skay.ua/uk/iphone-14/61509-apple-iphone-14-256gb-yellow.html",
+  "https://skay.ua/uk/iphone-14/58763-apple-iphone-14-512gb-blue.html",
+  "https://skay.ua/uk/iphone-14/58767-apple-iphone-14-512gb-product-red.html",
+  "https://skay.ua/uk/iphone-14/58765-apple-iphone-14-512gb-midnight.html",
+  "https://skay.ua/uk/iphone-14/58766-apple-iphone-14-512gb-starlight.html",
+  "https://skay.ua/uk/iphone-14/58764-apple-iphone-14-512gb-purple.html",
+  "https://skay.ua/uk/iphone-14/61510-apple-iphone-14-512gb-yellow.html",
+];
+const arrLinkJabko14 = [
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb--blue-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb--midnight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb-midnight",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb--starlight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb--pink-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-128gb--yellow-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--blue-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--midnight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--red-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--starlight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--pink-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-256gb--yellow-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--blue-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--midnight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--red-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--starlight-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--pink-",
+  "https://jabko.ua/rus/iphone/apple-iphone-14/apple-iphone-14-512gb--yellow-"
+]
+const arrLinkMobilePlanet = [
+  'https://mobileplanet.ua/apple-iphone-14-128gb-blue-mpvn3-254643',
+  'https://mobileplanet.ua/apple-iphone-14-128gb-product-red-mpva3-254640',
+  'https://mobileplanet.ua/apple-iphone-14-128gb-midnight-mpuf3-254634',
+  'https://mobileplanet.ua/apple-iphone-14-128gb-starlight-mpur3-254637',
+  'https://mobileplanet.ua/apple-iphone-14-128gb-purple-mpv03-254646',
+  'https://mobileplanet.ua/apple-iphone-14-128gb-yellow-mr3x3-269128',
+  
+  'https://mobileplanet.ua/apple-iphone-14-256gb-blue-mpwp3-254644',
+  'https://mobileplanet.ua/apple-iphone-14-256gb-product-red-mpwh3-254641',
+  'https://mobileplanet.ua/apple-iphone-14-256gb-midnight-mpvx3-254635',
+  'https://mobileplanet.ua/apple-iphone-14-256gb-starlight-mpw43-254638',
+  'https://mobileplanet.ua/apple-iphone-14-256gb-purple-mpwa3-254647',
+  'https://mobileplanet.ua/apple-iphone-14-256gb-yellow-mr3y3-268925',
+  
+  'https://mobileplanet.ua/apple-iphone-14-512gb-blue-mpxn3-254645',
+  'https://mobileplanet.ua/apple-iphone-14-512gb-product-red-mpxg3-254642',
+  'https://mobileplanet.ua/apple-iphone-14-512gb-midnight-mpww3-254636',
+  'https://mobileplanet.ua/apple-iphone-14-512gb-starlight-mpx33-254639',
+  'https://mobileplanet.ua/apple-iphone-14-512gb-purple-mpx93-254648',
+  'https://mobileplanet.ua/apple-iphone-14-512gb-yellow-mr513-272475'
+];
 
-async function f() {
-    const browser = await puppeteer.launch({headless: false})
-    const page = await browser.newPage()
-    await page.goto('https://skay.ua/uk/iphone-14/58753-apple-iphone-14-128gb-blue.html')
-    await page.screenshot({path: 'img.png'})
+  async function f() {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-    let arr = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-
-    await page.goto('https://skay.ua/uk/iphone-14/58757-apple-iphone-14-128gb-product-red.html')
-    let arr2 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-
-    await page.goto('https://skay.ua/uk/iphone-14/58755-apple-iphone-14-128gb-midnight.html')
-    let arr3 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
- 
-    await page.goto('https://skay.ua/uk/iphone-14/58756-apple-iphone-14-128gb-starlight.html')
-    let arr4 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-
-    await page.goto('https://skay.ua/uk/iphone-14/58754-apple-iphone-14-128gb-purple.html')
-    let arr5 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    for (let i = 0; i < arrLinkSkay14.length; i += 1) {
+      await page.goto(arrLinkSkay14[i]);
+      await page.screenshot({ path: "img.png" });
+  
+    let arr1 = await page.evaluate(() => {
+      let text = document.querySelector("#our_price_display").innerText;
+      let text2 = document.querySelector("h1").innerText;
+      return text2 + 'S: ' + text;
+    });
     
-    await page.goto('https://skay.ua/uk/iphone-14/61506-apple-iphone-14-128gb-yellow.html')
-    let arr6 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    console.log(arr1)
+    }
 
-    console.log(
-        'blue 128:',arr, ';',
-         'product-red 128:',arr2,';',
-          'midnight 128:',arr3,';',
-           'starlight 128:',arr4,';',
-            'purple 128:', arr5,';',
-             'yellow 128:',arr6,';'
-    )
-
-    await page.goto('https://skay.ua/uk/iphone-14/58758-apple-iphone-14-256gb-blue.html')
-    await page.screenshot({path: 'img.png'})
-
-    let arr7 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    for (let i = 0; i < arrLinkJabko14.length; i += 1) {
+      await page.goto(arrLinkJabko14[i]);
+      await page.screenshot({ path: "img.png" });
+  
+    let arr2 = await page.evaluate(() => {
+      let text = document.querySelector(".price-new__uah").innerText;
+      let text2 = document.querySelector("h1").innerText;
+      return text2 + 'J: ' + text;
+    });
     
-    await page.goto('https://skay.ua/uk/iphone-14/58762-apple-iphone-14-256gb-product-red.html')
-    let arr8 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    console.log(arr2)
+    }
     
-    await page.goto('https://skay.ua/uk/iphone-14/58760-apple-iphone-14-256gb-midnight.html')
-    let arr9 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-
-    await page.goto('https://skay.ua/uk/iphone-14/58761-apple-iphone-14-256gb-starlight.html')
-    let arr10 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    for (let i = 0; i < arrLinkMobilePlanet.length; i += 1) {
+      await page.goto(arrLinkMobilePlanet[i]);
+      await page.screenshot({ path: "img.png" });
+  
+    let arr3 = await page.evaluate(() => {
+      let text = document.querySelector(".price-value").innerText;
+      let text2 = document.querySelector("h1").innerText;
+      return text2 + 'MP: ' + text;
+    });
     
-    await page.goto('https://skay.ua/uk/iphone-14/58759-apple-iphone-14-256gb-purple.html')
-    let arr11 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
+    console.log(arr3)
+    }
     
-    await page.goto('https://skay.ua/uk/iphone-14/61509-apple-iphone-14-256gb-yellow.html')
-    let arr12 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-
-    console.log(
-        'blue 256:',arr7, ';',
-         'product-red 256:',arr8,';',
-          'midnight 256:',arr9,';',
-           'starlight 256:',arr10,';',
-            'purple 256:', arr11,';',
-             'yellow 256:',arr12,';'
-    )
-
-    await page.goto('https://skay.ua/uk/iphone-14/58763-apple-iphone-14-512gb-blue.html')
-    await page.screenshot({path: 'img.png'})
-
-    let arr13 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    
-    await page.goto('https://skay.ua/uk/iphone-14/58767-apple-iphone-14-512gb-product-red.html')
-    let arr14 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    
-    await page.goto('https://skay.ua/uk/iphone-14/58765-apple-iphone-14-512gb-midnight.html')
-    let arr15 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    
-    await page.goto('https://skay.ua/uk/iphone-14/58766-apple-iphone-14-512gb-starlight.html')
-    let arr16 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    
-    await page.goto('https://skay.ua/uk/iphone-14/58764-apple-iphone-14-512gb-purple.html')
-    let arr17 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    // console.log('purple 128: ',arr5)
-    await page.goto('https://skay.ua/uk/iphone-14/61510-apple-iphone-14-512gb-yellow.html')
-    let arr18 = await page.evaluate(()=>{
-        let text = document.querySelector('#our_price_display').innerText
-        return text
-    })
-    
-
-    console.log(
-        'blue 512:',arr13, ';',
-         'product-red 512:',arr14,';',
-          'midnight 512:',arr15,';',
-           'starlight 512:',arr16,';',
-            'purple 512:', arr17,';',
-             'yellow 512:',arr18,';'
-    )
-
-    await browser.close()
-}
-
-
+  }
+  f()
