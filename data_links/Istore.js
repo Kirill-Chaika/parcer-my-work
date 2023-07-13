6a2b7c2c9ae6afe32373c32ea4232f925c7f6f18
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-07-13 22:16:34 +0300

    ячсячсячсячс

---
+++ data_links/Istore.js
@@ -0,0 +1,134 @@
+const arrLinkIstore14 = [
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-128-gb-yellow/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-256-gb-yellow/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-512-gb-yellow/'
+  ];
+  const arrLinkIstore14Plus = [
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-128-gb-yellow/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-256-gb-yellow/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-starlight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-red/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-blue/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-midnight/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-plus-512-gb-yellow/'
+  ];
+  const arrLinkIstore14Pro = [
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-128-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-128-gb-graphite/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-128-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-128-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-256-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-256-gb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-256-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-256-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-512-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-512-gb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-512-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-512-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-1-tb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-1-tb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-1-tb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-1-tb-gold/'
+  ];
+  const arrLinkIstore14ProMax = [
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-128-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-128-gb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-128-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-128-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-256-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-256-gb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-256-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-256-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-512-gb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-512-gb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-512-gb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-512-gb-gold/',
+  
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-1-tb-deep-purple/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-1-tb-space-black/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-1-tb-silver/',
+    'https://www.istore.ua/catalog/item/apple-iphone-14-pro-max-1-tb-gold/'
+  ];
+  const arrLinkIstore14ProESIM = [
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-128-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-128-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-128-gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-128-gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-256-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-256-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-256-gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-256-gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-512-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-512-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-512-gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-512-gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-1-tb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-1-tb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-1-tb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-1-tb-esim-gold/'
+  ];
+  const arrLinkIstore14ProMaxESIM = [
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-128-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-128-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-128-gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-128-gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-256-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-256-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-256-gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-256-gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-512-gb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-512-gb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-512gb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-512gb-esim-gold/',
+  
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-1-tb-esim-deep-purple/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-1-tb-esim-space-black/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-1-tb-esim-silver/',
+    'https://www.istore.ua/ua/item/apple-iphone-14-pro-max-1-tb-esim-gold/'
+  ];
+  const IstoreDict = {
+    '14': arrLinkIstore14, '14Plus': arrLinkIstore14Plus, '14Pro': arrLinkIstore14Pro,
+    '14ProMax': arrLinkIstore14ProMax, '14ProESIM': arrLinkIstore14ProESIM, '14ProMaxESIM': arrLinkIstore14ProMaxESIM
+  };
+
+module.exports = IstoreDict;
